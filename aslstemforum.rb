8973e098501bfddfc3b3fe0f8a80130bc4f37c17
Giordon Stark <kratsg@gmail.com>
2013-10-13 00:05:59 -0500

    some bug fixes

--- aslstemforum.rb
+++ aslstemforum.rb
@@ -14,17 +14,17 @@ open('wordsOutput.out', 'a') do |f|
 
     break if $skipPage.length > 100
     unless response.code == "200" then
-      $skipPage.push({url: uri.to_s, code: response.code})
+      $skipPage.push({id: $i-1, code: response.code})
       next
     end
 
-    output = File.open("ASLStemForumData/%d.out" % ($1-1))
+    output = File.open("ASLStemForumData/%d.out" % ($i-1),'w+')
     output << response.body
     output.close
 
     response_doc = Nokogiri::HTML(response.body)
     sign = response_doc.css('#topic').text.gsub!(/Viewing sign ##{$i-1} for /,"")
     youtubeURL = response_doc.css('object.view_sign embed').attribute('src').value
-    f.puts('%d,%s,%s' % [$i, sign, youtubeURL])
+    f.puts('%d,%s,%s' % [$i-1, sign, youtubeURL])
   end
 end
